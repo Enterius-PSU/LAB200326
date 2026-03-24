220734291b91158fb32cb3439704c8d8e25f83c0
Aleksandr. K. <kushev231@gmail.com>
2026-03-24 09:24:33 +0500

    Update Program.fs
    Одновил tree.Fold()

--- LAB200326_2/Program.fs
+++ LAB200326_2/Program.fs
@@ -1,4 +1,4 @@
-﻿// Кушев Александр БАС-2
+// Кушев Александр БАС-2
 // Лабораторная работа №4 (Задание №2)
 
 open System
@@ -38,17 +38,15 @@ let rec insert tree newValue =
     | None -> 
         {value = newValue; left = None; right = None}
 
-// Подсчёт элементов, не содержащих заданную цифру
-let rec searchDigitInTree tree digit =
+// tree.Fold()
+let rec treeFold funcToApply acc tree =
     match tree with
+    | None -> acc
     | Some t ->
-        let leftCount = searchDigitInTree t.left digit
-        let rightCount = searchDigitInTree t.right digit
-        if isDigitInNumber t.value digit then
-            leftCount + rightCount
-        else
-            leftCount + rightCount + 1
-    | None -> 0
+        let newAcc = funcToApply acc t.value
+        let leftAcc = treeFold funcToApply newAcc t.left
+        let rightAcc = treeFold funcToApply leftAcc t.right
+        rightAcc
 
 // Вывод бинарного дерева
 let rec printTree tree indent side =
@@ -71,7 +69,7 @@ let rec printTree tree indent side =
 
 // Функция генерации уникального числа
 let rec uniqueNumber usedNumbers =
-    let newNumber = Random().Next(-100, 100)
+    let newNumber = System.Random().Next(-100, 100)
     if List.contains newNumber usedNumbers then
         uniqueNumber usedNumbers
     else
@@ -90,7 +88,7 @@ let rec createTree count numbers =
 // Запрос и проверка цифры
 let rec checkDigit() = 
     printf "Введите цифру для поиска: "
-    let digit = int(Console.ReadLine())
+    let digit = int(System.Console.ReadLine())
     if digit >= 0 && digit <= 9 then
         digit
     else
@@ -100,7 +98,7 @@ let rec checkDigit() =
 // Запрос и проверка количества элементов
 let rec checkCount() = 
     printf "Введите количество элементов бинарного дерева: "
-    let count = int(Console.ReadLine())
+    let count = int(System.Console.ReadLine())
     if count < 0 then
         printfn "Количество элементов < 0!\n"
         checkCount()
@@ -114,9 +112,16 @@ let main args =
     printf "Исходное дерево: \n%s\n" (printTree tree "" "")
 
     let digit = checkDigit()
-    let result = searchDigitInTree tree digit
+
+    let result = treeFold (
+        fun acc value -> 
+            if isDigitInNumber value digit then 
+                acc 
+            else 
+                acc + 1) 
+                    0 tree
+
     printf "Количество элементов, не содержащих цифру %i: %i" digit result
 
     System.Console.ReadKey() |> ignore
     0
-
